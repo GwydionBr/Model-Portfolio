e0c8b6d9e1314313c8aae7fe6b3f1ddc8fc2eced
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-06-16 17:03:36 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+

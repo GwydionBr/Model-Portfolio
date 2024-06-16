c2b7e788fa74bd5f83632390cab6b888cf820e60
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-06-16 17:19:40 +0200

    chore: Update .gitignore and package.json

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import env from 'dotenv';
 
 const app = express();
+const port = 3000;
+env.config();
+
+const title = process.env.TITLE || 'Title';
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.ejs', { title });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
